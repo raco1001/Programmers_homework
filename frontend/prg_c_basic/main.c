8e0d5a2d8d234f00aea9abc75a26af794ba9c946
김종현 <jhk9027@gmail.com>
2025-04-01 19:52:08 +0900

    c: pointer

--- frontend/prg_c_basic/main.c
+++ frontend/prg_c_basic/main.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-    char input;
-    int scan_result; 
-    
-    printf("문자를 입력하세요: ");
-    scan_result = scanf("%c", &input);
-    
-    if (scan_result != 1) {
-        printf("입력 오류가 발생했습니다.\n");
-        return 1;  
-    }
-    
-    printf("입력한 문자는 %c, 아스키코드는 %d입니다.\n", input, input);
-    
-    char *ptr = &input; 
-    printf("입력한 문자의 메모리 주소는 %p입니다.\n", (void*)ptr);
-    
-    return 0;
+
+void function(int *pArray){
+  for(int i = 0; i < 5; i++){
+    printf("함수 내부의 배열의 요소 pArray[%d]: %d\n", i, *(pArray + i));
+  }
 }
+
+int main() {
+  int array[] = {1,2,3,4,5};
+  function(array);
+  return 0;
+}
+
