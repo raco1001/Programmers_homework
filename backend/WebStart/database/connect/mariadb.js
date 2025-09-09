6f040adcb134ca15c2ab3aa8bd64472d56d4c160
김종현 <jhk9027@gmail.com>
2025-09-09 21:17:29 +0900

    fix: .gitignore & notes-db.yml - limit resources

--- backend/WebStart/database/connect/mariadb.js
+++ backend/WebStart/database/connect/mariadb.js
@@ -1,11 +1,11 @@
-const mariadb = require("mysql");
+const mariadb = require('mysql')
 
 const conn = mariadb.createConnection({
-  host: "218.234.14.81",
+  host: 'localhost',
   port: 3307,
-  user: "root",
-  password: "root",
-  database: "Tennis",
-});
+  user: 'root',
+  password: 'root',
+  database: 'Tennis',
+})
 
-module.exports = conn;
+module.exports = conn
